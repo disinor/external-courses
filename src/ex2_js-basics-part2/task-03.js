df20a78cad473b2fd550a53d08d52ae5037fa061
Disinor <apikulin437@gmail.com>
2020-09-28 19:30:34 +0300

    complete

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,19 +1,24 @@
-let even = 0;
-let notEven = 0;
-let zero = 0;
 function checkEven(e) {
+  let even = 0;
+  let notEven = 0;
+  let zero = 0;
+
   e.forEach((element) => {
     if (typeof element !== "number") {
       console.log("В массиве есть не число");
-    } else if (element === 0) {
-      zero + 1;
-    } else if (element % 2 === 0) {
-      even++;
-    } else if (element % 2 !== 0) {
-      notEven++;
+    }
+    if (element === 0) {
+      zero += 1;
+    }
+    if (element % 2 === 0) {
+      even+=1;
+    }
+    if (element % 2 !== 0) {
+      notEven+=1;
     }
   });
 
- return `Четных:${even}; Нечетных:${notEven}; Нулей:${zero}`
+  return `${even}${notEven}${zero}`;
 }
+
 module.exports = checkEven();
--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,14 +1,14 @@
-
-
 function numCheck(e) {
-  if (typeof e == "number" && e <= 1000) {
-    if (e != 0 && e != 1) {
-      if (e % 2 != 0) {
+  if (typeof e === "number" && e <= 1000) {
+    if (e !== 0 && e !== 1) {
+      if (e % 2 !== 0) {
         console.log("простое");
-      } else if (e % 2 == 0) {
+      } else if (e % 2 === 0) {
         console.log("Составное ");
       }
-    }else {console.log('0 и 1 не являются простым или составным числом')}
+    } else {
+      console.log("0 и 1 не являются простым или составным числом");
+    }
   } else {
     console.log("больше 1000 не работаем)");
   }
