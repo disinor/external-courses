2262d599f6caba271dd7e683876c70478e42e5f0
disinor <apikulin437@gmail.com>
2020-11-12 21:38:52 +0300

    работа над ошибками

--- src/kanban/kanban.js
+++ src/kanban/kanban.js
@@ -1,53 +1,41 @@
-const header2 = document.querySelector('.header2')
-const newDiv =document.createElement("div");
-newDiv.dataset.active='false'
-const newUl=document.createElement('ul');
-const newLi1=document.createElement('li');
-const newLi2=document.createElement('li');
-const newLi3=document.createElement('li');
-const newA1=document.createElement('a')
-const newA2=document.createElement('a')
-const newA3=document.createElement('a')
-newDiv.className="header2_arrow_settings";
+const header2 = document.querySelector(".header2");
+const newDiv = document.createElement("div");
+const newUl = document.createElement("ul");
+const newLi1 = document.createElement("li");
+const newLi2 = document.createElement("li");
+const newLi3 = document.createElement("li");
+const newA1 = document.createElement("a");
+const newA2 = document.createElement("a");
+const newA3 = document.createElement("a");
+const dropDawn = document.querySelector(".header2 .header2_arrow_settings");
+const blockLogo = document.querySelector(".blockLogo");
+const arrow = document.querySelector(".arrow");
+
+newDiv.className = "header2_arrow_settings";
 newDiv.append(newUl);
 
-newUl.append(newLi1)
-newLi1.append(newA1)
-newA1.innerText='My account'
+newUl.append(newLi1);
+newLi1.append(newA1);
+newA1.innerText = "My account";
 
-newUl.append(newLi2)
-newLi2.append(newA2)
-newA2.innerText='My tasks'
+newUl.append(newLi2);
+newLi2.append(newA2);
+newA2.innerText = "My tasks";
 
+newUl.append(newLi3);
+newLi3.append(newA3);
+newA3.innerText = "Log out.";
 
-newUl.append(newLi3)
-newLi3.append(newA3)
-newA3.innerText='Log out.'
+arrow.dataset.activeArrow = "false";
 
-
-header2.append(newDiv);
-
-const dropDawn=document.querySelector('.header2 .header2_arrow_settings')
-const arrow= document.querySelector('.arrow')
-arrow.addEventListener('click',()=>{
-if(dropDawn.dataset.active == 'false'){
-    dropDawn.dataset.active='true'
-    dropDawn.style.display="inline"
-    arrow.style.transform="rotate(-224.5deg)"
-}
-else{
-    dropDawn.dataset.active='false'
-    dropDawn.style.display="none"
-    arrow.style.transform="rotate(-45deg)"
-}
-})
-
-
-
-
-
-
-
-
-
-
+blockLogo.addEventListener("click", () => {
+  if (arrow.dataset.activeArrow == "false") {
+    arrow.dataset.activeArrow = "true";
+    header2.append(newDiv);
+    arrow.style.transform = "rotate(-224.5deg)";
+  } else {
+    arrow.dataset.activeArrow = "false";
+    newDiv.remove();
+    arrow.style.transform = "rotate(-45deg)";
+  }
+});
