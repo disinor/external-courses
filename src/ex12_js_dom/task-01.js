920f016a88985f878d01885e28e0c19e731e5d6b
disinor <apikulin437@gmail.com>
2020-11-12 23:01:43 +0300

    осталось разобраться с анимациямми

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,47 +1,56 @@
-let currentImg = 2;
-
 const rightButton = document.querySelector(".buttonRight");
-leftButton = document.querySelector(".buttonLeft");
+const leftButton = document.querySelector(".buttonLeft");
 
 function positionImg() {
   const leftImg = document.querySelector(".leftImage");
   const centerImg = document.querySelector(".centerImage");
   const rightImg = document.querySelector(".rightImage");
-/*   const oneChild =document.querySelector('.myImage:nth-child(1)')
-  const twoChild =document.querySelector('.myImage:nth-child(2)')
-  const thirdCild =document.querySelector('.myImage:nth-child(3)')
-  return { leftImg, centerImg, rightImg ,oneChild,twoChild,thirdCild}; */
-}
 
-rightButton.addEventListener("click", () => {
-    const { leftImg, centerImg, rightImg ,oneChild,twoChild,thirdCild } = positionImg();
+  return { leftImg, centerImg, rightImg };
+}
+let currentImg = 2;
+leftButton.addEventListener("click", () => {
+  const { leftImg, centerImg, rightImg } = positionImg();
 
   leftImg.classList.remove("leftImage");
-  centerImg.classList.remove("centerImage");
-  rightImg.classList.remove("rightImage");
-
   leftImg.classList.add("centerImage");
+
+  centerImg.classList.remove("centerImage");
   centerImg.classList.add("rightImage");
+
+  rightImg.classList.remove("rightImage");
   rightImg.classList.add("leftImage");
 
+/*   leftImg.classList.add("leftImageToCenter");
+  centerImg.classList.add("centerImageToRight");
+  rightImg.classList.add("rightImageToLeft"); */
 
-  console.log(twoChild);
-  twoChild.style.background = `url(asset/img${currentImg}.jpg)`;
+  leftImg.classList.remove();
+  centerImg.classList.remove();
+  rightImg.classList.remove();
+  
+  if (currentImg >= 6) {
+    currentImg = 0;
+  }
   currentImg = currentImg + 1;
+  leftImg.style.background = `url(asset/img${currentImg}.jpg)`;
 });
 
-
-leftButton.addEventListener("click", () => {
-  const { leftImg, centerImg, rightImg ,oneChild,twoChild,thirdCild } = positionImg();
-  
+rightButton.addEventListener("click", () => {
+  const { leftImg, centerImg, rightImg } = positionImg();
+  console.log(1);
   leftImg.classList.remove("leftImage");
-  centerImg.classList.remove("centerImage");
-  rightImg.classList.remove("rightImage");
-
-
   leftImg.classList.add("rightImage");
+
+  centerImg.classList.remove("centerImage");
   centerImg.classList.add("leftImage");
+
+  rightImg.classList.remove("rightImage");
   rightImg.classList.add("centerImage");
 
-
+  if (currentImg <= 1) {
+    currentImg = 7;
+  }
+  currentImg = currentImg - 1;
+  rightImg.style.background = `url(asset/img${currentImg}.jpg)`;
 });
