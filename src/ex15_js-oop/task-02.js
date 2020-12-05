32fe2203a70a61e84f2ee8cb3f495017c5486195
disinor <apikulin437@gmail.com>
2020-12-05 17:35:57 +0300

    eddit task-02

--- src/ex15_js-oop/task-02.js
+++ src/ex15_js-oop/task-02.js
@@ -1,12 +1,72 @@
-const Apartament = function (nameApartament) {
-  this.name = nameApartament;
+const NewRoom = function (nameRoom) {
+  this.nameRoom = nameRoom;
+  this.shine = false;
+  this.devices = [];
+
+  this.toTurnOnTheLight = () => {
+    this.shine = true;
+  };
+  this.toTurnOffTheLight = () => {
+    this.shine = false;
+  };
+  this.addDevice = (device) => {
+    this.devices.push(device);
+  };
 };
 
-const NewDevice = function (nameDevice, power, room) {
-  this.nameDevice = nameDevice;
-  this.power = power;
-  this.switchPosition = false;
+const newRoomHall = function (nameRoom) {
+  NewRoom.apply(this, arguments);
+  this.floorHeating = false;
+  this.power = 5;
+
+  this.floorHeatingOn = () => {
+    this.floorHeating = true;
+  };
+
+  this.floorHeatingOff = () => {
+    this.floorHeating = false;
+  };
+};
+
+const Apartament = function (nameApartament) {
+  this.nameApartament = nameApartament;
+  this.roomApartament = [];
+
+  this.addRoom = (nameRoom) => {
+    this.roomApartament.push(nameRoom);
+  };
+
+  this.calculateThePower = function () {
+    this.powerConsumption = 0;
+    
+    apartament.roomApartament.forEach((e) => {
+      e.devices.forEach((element) => {
+        if (element.switchPosition === true) {
+          console.log(this.powerConsumption);
+          this.powerConsumption += element.powerDevice;
+        }
+      });
+    });
+  };
+
+  this.searchDevise = function (name) {
+    this.powerConsumption = 0;
+
+    apartament.roomApartament.forEach((e) => {
+      e.devices.forEach((element) => {
+        if (element.nameDevice === name) {
+          console.log(element);
+        }
+      });
+    });
+  };
+};
+
+const NewDevice = function (name, power, room) {
+  this.nameDevice = name;
+  this.powerDevice = power;
   this.roomDevice = room;
+  this.switchPosition = false;
 
   this.turnOn = function () {
     this.switchPosition = true;
@@ -17,66 +77,60 @@ const NewDevice = function (nameDevice, power, room) {
   };
 };
 
-const Room = function (nameRoom) {
-  this.room = nameRoom;
+const NewDeviceTv = function (name, power, room, volume) {
+  NewDevice.apply(this, arguments);
+  this.volume = volume;
+  this.turnUpTheVolume = () => {
+    this.volume += 5;
+  };
 
-  this.addDevice = function (nameDevice, power, room) {
-    this[nameDevice] = new NewDevice(nameDevice, power, room);
+  this.turnDownTheVolume = () => {
+    this.volume -= 5;
   };
 };
 
-const rooms = {
-  kitchen: new Room('kitchen'),
-  hall: new Room('hall'),
-  bedroom: new Room('bedroom'),
+const newDeviceLamp = function (name, power, room, color) {
+  NewDevice.apply(this, arguments);
+  this.color = color;
+  this.changeColor = (newColor) => {
+    this.color = newColor;
+  };
 };
 
-Apartament.prototype = rooms;
+const newDeviceIron = function (name, power, room, heatingPower) {
+  NewDevice.apply(this, arguments);
+  this.heatingPower = heatingPower;
 
-rooms.kitchen.addDevice('tvKitchen', 10, 'kitchen');
-rooms.kitchen.addDevice('microwaveOven', 15, 'kitchen');
-rooms.kitchen.addDevice('fridge', 13, 'kitchen');
-rooms.kitchen.microwaveOven.turnOn();
-
-rooms.bedroom.addDevice('iron', 13, 'bedroom');
-rooms.bedroom.addDevice('lamp', 3, 'bedroom');
-
-rooms.hall.addDevice('tvHall', 18, 'hall');
-rooms.hall.addDevice('ps', 5, 'hall');
-rooms.hall.tvHall.turnOn();
-rooms.hall.ps.turnOn();
-
-const newSearch = function (name) {
-  for (room in rooms) {
-    for (device in rooms[room]) {
-      let deviceRoom = rooms[room][device];
-
-      if (typeof deviceRoom === 'object') {
-        if (deviceRoom.nameDevice === name) {
-          return deviceRoom;
-        }
-      }
-    }
-  }
-  return undefined
-};
-
-const powerConsumption = function () {
-  let sum = 0;
-
-  for (room in rooms) {
-    for (device in rooms[room]) {
-      let deviceRoom = rooms[room][device];
-
-      if (typeof deviceRoom === 'object') {
-        if (deviceRoom.switchPosition === true) {
-          sum += deviceRoom.power;
-        }
-      }
-    }
-  }
-
-  return sum;
+  this.increaseHeating = (value) => {
+    this.heatingPower = value;
+  };
 };
 
 const apartament = new Apartament('Квартира Васи');
+apartament.addRoom(new newRoomHall('Hall'));
+apartament.addRoom(new NewRoom('kitchen'));
+apartament.addRoom(new NewRoom('beadroom'));
+
+apartament.roomApartament[0].addDevice(new NewDeviceTv('tv', 18, 'hall', 25));
+apartament.roomApartament[0].addDevice(new NewDevice('ps', 5, 'hall'));
+apartament.roomApartament[0].devices[0].turnOn();
+/* apartament.roomApartament[0].devices[1].turnOn(); */
+
+apartament.roomApartament[1].addDevice(
+  new NewDevice('microvave', 8, 'kitchen')
+);
+apartament.roomApartament[1].addDevice(
+  new newDeviceLamp('lamp', 3, 'kitchen', 'red')
+);
+
+apartament.roomApartament[2].addDevice(
+  new newDeviceIron('iron', 10, 'beadroom', 64)
+);
+apartament.roomApartament[2].addDevice(
+  new newDeviceLamp('lamp', 3, 'beadroom', 'green')
+);
+apartament.roomApartament[2].devices[0].turnOn();
+
+apartament.searchDevise('lamp');
+
+console.log(apartament);
