2d5496d0e0b93ffdc3aa86d5f99f979092991cfc
disinor <apikulin437@gmail.com>
2020-12-05 20:47:30 +0300

    add task-01

---
+++ src/ex16_js-async/task-01.js
@@ -0,0 +1,29 @@
+const newObjUser = {
+    name:'Sasha',
+    age:25,
+
+}
+const url = 'https://jsonplaceholder.typicode.com/users'
+function request(method, url,body =null) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type','application/json')
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+    xhr.send(JSON.stringify(body))
+  });
+}
+
+request('GET',url).then(data=>console.log(data))
+request('POST',url,newObjUser).then(data=>console.log(data))
+
+
